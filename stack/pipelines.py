4eae5e252249902ad1d109da182f70a8e5d3ad26
Samur18 <105276323+Samur18@users.noreply.github.com>
2024-03-23 13:15:05 +0100

    db

--- stack/pipelines.py
+++ stack/pipelines.py
@@ -5,9 +5,28 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import logging
+from scrapy.exceptions import DropItem
+from scrapy.utils.project import get_project_settings
+import pymongo
 
 
-class StackPipeline:
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        settings = get_project_settings()
+        uri = settings.get("MONGODB_URI")
+        self.client = pymongo.MongoClient(uri)
+        db = self.client.get_database(settings.get("MONGODB_DB"))
+        self.collection = db.get_collection(settings.get("MONGODB_COLLECTION"))
+
     def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert_one(dict(item))
+            logging.debug("Question added to MongoDB database!")
         return item
